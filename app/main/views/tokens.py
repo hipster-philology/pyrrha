8ff337eebc2e3cbe6ca089ae481287831db77c20
Thibault Clérice <leponteineptique@gmail.com>
2019-02-28 11:37:04 +0100

    Fix previous fix on #113 that added a new bug

--- app/main/views/tokens.py
+++ app/main/views/tokens.py
@@ -255,7 +255,7 @@ def tokens_search_through_fields(corpus_id):
             branch_filters.extend(column_search_filter(getattr(WordToken, name), value))
 
         value_filters.append(branch_filters)
-    else:  # If the search is empty, we only search for the corpus_id
+    if not value_filters:  # If the search is empty, we only search for the corpus_id
         value_filters.append([WordToken.corpus == corpus_id])
 
     # there is at least one OR clause
