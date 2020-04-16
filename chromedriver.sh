83881e2356a6bd9785de910bc83bd0ae17ff0e44
Thibault Clérice <leponteineptique@gmail.com>
2020-04-16 12:06:13 +0200

    (Requirements) Upgrade versions

--- chromedriver.sh
+++ chromedriver.sh
@@ -1,6 +1,7 @@
-wget -N https://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/share/
 sudo chmod +x /usr/local/share/chromedriver
+sudo rm /usr/local/bin/chromedriver
 sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
