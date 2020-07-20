0b7e192fcb20dd5ccee8b72b208d13a4e2e39458
Carine Dengler <carine-dengler@logilab.fr>
2020-07-20 15:43:13 +0200

    show detailed error message when duplicates are in list of allowed lemmas (closes #175)
    show detailed message containing duplicates in user interface, but
    hide possibly more cryptic exceptions (database connection errors, ...)
    from user

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,8 @@
 from .pagination import int_or
 from .tsv import StringDictReader
 from .forms import string_to_none
+
+
+class PyrrhaError(Exception):
+    """Raised when errors specific to this application occur."""
+    pass
