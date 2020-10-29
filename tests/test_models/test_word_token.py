8af86a9f0c427cd2374e1e5bd5dc19422f97a8e9
François Ferry <francois.ferry@logilab.fr>
2020-10-29 14:20:11 +0100

    ad a test for updating left and right context

--- tests/test_models/test_word_token.py
+++ tests/test_models/test_word_token.py
@@ -42,3 +42,24 @@ class TestWordToken(TestModels):
             for i in range(64)
         )
         self.assertEqual(WordToken.add_batch(0, [{"form": form}]), 1)
+
+    def test_update_batch_context(self):
+        """Test updating left and right context.
+
+        Trying: set right and left context to 4.
+        """
+        form_list = [
+            {
+                "form": "".join(
+                    string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)]
+                    for i in range(16)
+                )
+            } for j in range(200)
+        ]
+        WordToken.add_batch(0, form_list)
+        self.assertEqual(WordToken.update_batch_context(0, 4, 4), 200)
+        token = WordToken.query.filter_by(corpus=0, order_id=15).first()
+        left_context = token.left_context.split(" ")
+        right_context = token.right_context.split(" ")
+        self.assertEqual(len(left_context), 4)
+        self.assertEqual(len(right_context), 4)
