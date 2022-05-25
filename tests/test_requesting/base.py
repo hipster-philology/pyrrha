57cb8577e05de00dd55b0a285ec3c19ca57f318c
Carine Dengler <carine-dengler@logilab.fr>
2022-05-25 15:21:39 +0200

    fix: create database if it does not exist

--- tests/test_requesting/base.py
+++ tests/test_requesting/base.py
@@ -4,6 +4,7 @@ from flask import url_for
 
 from app.models import User, Role
 from app import create_app, db
+from sqlalchemy_utils import database_exists, create_database
 
 
 class TestBase(TestCase):
@@ -19,6 +20,8 @@ class TestBase(TestCase):
         """
         Will be called before every test
         """
+        if not database_exists(db.engine.url):
+            create_database(db.engine.url)
         self.app = self.create_app()
         self.client = self.app.test_client()
         db.session.commit()
