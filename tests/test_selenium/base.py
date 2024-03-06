75c04d58fa157d74c31545ca47b235262b1a9059
Juliettejns <juliette.janes@yahoo.fr>
2024-03-06 17:21:59 +0100

    ajout morph lorem ipsum

--- tests/test_selenium/base.py
+++ tests/test_selenium/base.py
@@ -216,7 +216,7 @@ class TestBase(LiveServerTestCase):
         self.create_driver()
         self.driver.get(self.get_server_url())
 
-    LOREM_IPSUM = """tokens	lemmas	pos
+    LOREM_IPSUM = """tokens	lemmas	pos morph
 Lorem
 ipsum           
 dolor           
