8da0aa9010d739956ce631f85d74a986b54e9cc1
Juliettejns <juliette.janes@yahoo.fr>
2024-03-11 15:49:08 +0100

    ajout lower à email suite à test failed pour adresse déjà utilisée

--- app/account/forms.py
+++ app/account/forms.py
@@ -44,7 +44,7 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
+        if User.query.filter_by(email=field.data.lower()).first():
             raise ValidationError('Email already registered. (Did you mean to '
                                   '<a href="{}">log in</a> instead?)'.format(
                 url_for('account.login')))
